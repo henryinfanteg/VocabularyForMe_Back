a3e0adf587374cef3a19db1496643cf27f18480f
Henry Daniel Infante Gutierrez <henryinfanteg@gmail.com>
2021-02-14 02:21:43 -0500

    change db info

--- keys.js
+++ keys.js
@@ -1,5 +1,11 @@
 module.exports = {
     database: {
+        host: 'db-words-english-1.cluster-cwktkmlttgpr.us-east-2.rds.amazonaws.com',
+        user: 'hengadm',
+        password: 'TfgPavNoyYocaQD5dnz9',
+        database: ''
+    },
+    databaseLocal: {
         host: 'localhost',
         user: 'u_wordsen',
         password: 'Hector21902421@',
